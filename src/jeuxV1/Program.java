f8936749e2f99d96b66fd5a9ecbeb6aee17d56be
ilyassking <ilyas.ariba@gmail.com>
2018-12-11 22:23:49 +0000

    Adding Score

--- src/jeuxV1/Program.java
+++ src/jeuxV1/Program.java
@@ -6,27 +6,36 @@ import java.util.List;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
-
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Program extends Application {
-
-//Les élements de l'interface graphique
+	private GridPane  gp = new GridPane();
+	private static int nb=0;
+	private static int x=0;
+	private static Text nbal= new Text("Balles tires : "+nb);
+	private static Text nmonst= new Text("   |   Monstres tués : " + x);
+	//Les élements de l'interface graphique
 	private double widthWindow=800;
 	private double heightWindow=600;
 	private Pane container=new Pane();
 	Line line = new Line(0, 200, widthWindow, 200);
-	Zone zone1=new Zone(0,0,line.getEndX()-50,line.getEndY()-50);
+	Zone zone1=new Zone(0,20,line.getEndX()-50,line.getEndY()-50);
 	Zone zone2=new Zone(line.getStartX(),line.getStartY(),line.getEndX()-50,heightWindow-50);
-//les objets du jeu
+	//les objets du jeu
 	private Player player = new Player(zone2);
 	private List<Monster> monsters = new ArrayList<>();
 	private List<Balle> balles = new ArrayList<>();
@@ -34,9 +43,8 @@ public class Program extends Application {
 	
 	public Rectangle porte = new Rectangle(widthWindow-26, 350, 10, 90);
 	private List<Man> mans = new ArrayList<>();
-	private double xCof = 1;
-	private double yCof = 1;
-//AnimationTimer
+	
+	//AnimationTimer
 	AnimationTimer animation=new AnimationTimer(){
 
 		@Override
@@ -64,12 +72,12 @@ public class Program extends Application {
 				Balle balle = new Balle(arme);
 				container.getChildren().add(balle.getCorps());
 				balles.add(balle);
+				nb++;
+				nmonst.setText("   |   Monstres tués : "+nb);
 			}
 			if(event.getCode()==KeyCode.UP){
 				player.getCorps().setTranslateY(player.getCorps().getTranslateY()-5);
 				arme.attachToPlayer(player);
-				
-
 			}
 			if(event.getCode()==KeyCode.DOWN){
 				player.getCorps().setTranslateY(player.getCorps().getTranslateY()+5);
@@ -101,6 +109,8 @@ public class Program extends Application {
 					container.getChildren().removeAll(balle.getCorps(),monstre.getCorps(), monstre.b.getCorps());
 					balle.setAlive(false);
 					monstre.setAlive(false);
+					x++;
+					nbal.setText("Balles tires : " + x);
 				}
 			}
 		}
@@ -120,15 +130,13 @@ public class Program extends Application {
 			monsters.add(m);
 		}
 		if(Math.random()<0.0099) {
-//			Man man = new Man(10, Tools.getRandom(line.getStartY(),widthWindow), 30, 30);
 			Man man = new Man(zone2);
 			container.getChildren().add(man.getCorps());
 			mans.add(man);
-//			man.setX(man.getX()+Math.random()*0.9);
 			man.getCorps().setTranslateX(man.getCorps().getTranslateX()+Math.random()*0.9);
-			if(Math.random()<0.1)
+			if(Math.random()<0.1) {
 				man.getCorps().setTranslateY(man.getCorps().getTranslateY()-Math.random()*7);
-//			man.setY(man.getY()-Math.random()*7);
+			}
 		}
 		for(Man r:mans){
 			r.getCorps().setTranslateX(r.getCorps().getTranslateX()+Math.random()*0.9+Math.random());
@@ -137,7 +145,7 @@ public class Program extends Application {
 			}else {
 				r.getCorps().setTranslateY(r.getCorps().getTranslateY()+0.19);
 			}
-			// remove Man if X>Window width
+			// remove Man if X > Window width
 			if(r.getCorps().getTranslateX()>widthWindow-40) {
 				container.getChildren().remove(r.getCorps());
 				r.setAlive(false);
@@ -154,6 +162,7 @@ public class Program extends Application {
 	}
 
 	private void createContent(){
+		container.getChildren().add(gp);
 		container.getChildren().add(line);
 		porte.setFill(Color.BROWN);
 		container.getChildren().add(porte);
@@ -161,20 +170,39 @@ public class Program extends Application {
 		container.getChildren().add(arme.getCorps());
 		container.getChildren().add(arme.getSortie());
 	}
+	
+	class topBar extends HBox {
+        public topBar() {
+        	nbal.setFont(Font.font("Verdana", FontWeight.NORMAL, 13));
+        	nbal.setFill(Color.WHITE);
+        	nmonst.setFont(Font.font("Verdana", FontWeight.NORMAL, 13));
+        	nmonst.setFill(Color.WHITE);
+            this.getChildren().addAll(nbal,nmonst);
+            this.setStyle("-fx-padding: 5 0 5 10;-fx-background-color: #7fb0ff;");
+            this.setMinWidth(widthWindow);
+        }
+    }
+	
 	@Override
 	public void start(Stage window) throws Exception {
 		window.setWidth(widthWindow);
 		window.setHeight(heightWindow);
 		window.setTitle("jeu de guerre");
-		createContent();
+		createContent();		
+		
+		HBox toolBar = new HBox();
+		toolBar.setAlignment(Pos.CENTER_RIGHT);
+        toolBar.setPrefHeight(25);
+        toolBar.setMinHeight(25);
+        toolBar.setMaxHeight(25);
+        toolBar.getChildren().add(new topBar());
+        container.getChildren().add(toolBar);
 		
 		Scene scene=new Scene(container);
-		
 		window.setScene(scene);
 		animation.start();
 		scene.setOnKeyPressed(event);
 		window.show();
-		
 	}
 
 }
